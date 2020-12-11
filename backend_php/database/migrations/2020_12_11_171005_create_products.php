2d28f728ffe290b367ccadcaf61f9fa6a331e7b4
Agredas <andreagredas@gmail.com>
2020-12-11 19:12:17 +0100

    Table Migrations created.

---
+++ backend_php/database/migrations/2020_12_11_171005_create_products.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProducts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->string('name');
+            $table->string('description');
+            $table->decimal('price',8,2);
+            $table->string('image')->nullable();
+            $table->timestamps();
+
+            $table->foreignId('category_id')->constrained('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
---
+++ backend_php/database/migrations/2020_12_11_172505_create_orders.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrders extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('status');
+            $table->deliveryDate('date')->addDays(3);
+            $table->integer('user_id');
+            $table->timestamps();
+
+            $table->foreignId('user_id')->constrained('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
