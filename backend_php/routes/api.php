ada117257a9e21a5932ef701328f93fefd60a81d
Agredas <andreagredas@gmail.com>
2020-12-12 20:15:59 +0100

    Adding Register, Login and Logout routes.

--- backend_php/routes/api.php
+++ backend_php/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/', function(){
+    return response()->json(['message' => 'Running smoothly!'], 200);
 });
+
+Route::post('user/register', [UserController::class,'store']);
+Route::post('user/login', [UserController::class,'login'])->name('login');
+
+Route::group(['middleware' => ['auth:api']], function () {    
+    Route::post('user/logout', [UserController::class,'logout']);
+});
+
