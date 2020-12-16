33e40e9f1be9087413883ee065227cf8faed633f
Agredas <andreagredas@gmail.com>
2020-12-16 17:31:08 +0100

    Problem in Admin role solved.

--- backend_php/app/Http/Middleware/Role.php
+++ backend_php/app/Http/Middleware/Role.php
@@ -10,7 +10,7 @@ class Role{
     //User must be authenticated
     $user = Auth::user();
 
-    if(in_array($user->rol, $roles))
+    if(in_array($user->role, $roles))
       return $next($request);
       return response()->json(['error' => "You don't have access to this resource."], 403);
   }
