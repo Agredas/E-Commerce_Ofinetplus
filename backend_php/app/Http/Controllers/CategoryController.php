9ad9ab67949983bb8b8beabc1ef0b29c5ce0b563
Agredas <andreagredas@gmail.com>
2020-12-16 12:24:03 +0100

    Functions indexAll, Store and GetByName created.

--- backend_php/app/Http/Controllers/CategoryController.php
+++ backend_php/app/Http/Controllers/CategoryController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function indexAll(){
+        $categories = Category::with('products')->get();
+        return response($categories);
+    }
+
+    public function store(Request $request){
+        $body = $request->all();
+        $category = Category::create($body);
+        return response($category);
+    }
+
+    public function getByName($name){              
+        $category = Category::with('products')->find($name);                             
+        return $category;
+    }
 }
